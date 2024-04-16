7b636b44159f1eb045b4bfe35e62ab62a3b56f0e
Macbook <jackie@dast.tw>
2024-04-17 01:42:24 +0800

    重新編排：重新編排並優化源代碼操作
    - 在源設置中移除有關編排代碼操作的引用

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,7 +5,6 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.code_actions.refactoring,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.shfmt,
