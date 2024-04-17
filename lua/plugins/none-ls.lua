1355402736d445e190d79b9058061f6c37a3ceb7
HomePC-WSL <jackie@dast.tw>
2024-04-17 21:31:19 +0800

    功能: 整合代碼操作和診斷自動完成
    - 移除未使用的 `null_ls` 自動完成
    - 將 `gitsigns` 加入作為代碼操作
    - 將 `ansiblelint` 加入作為一項診斷

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,24 +1,21 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.shfmt,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.shfmt,
         null_ls.builtins.code_actions.gitsigns,
-        null_ls.builtins.code_actions.refactoring,
-        null_ls.builtins.completion.luasnip,
-        null_ls.builtins.completion.spell,
         null_ls.builtins.diagnostics.ansiblelint,
         null_ls.builtins.diagnostics.hadolint,
         null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.yamllint,
-			},
-		})
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
