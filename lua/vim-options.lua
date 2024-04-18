20da29abb73edb6cfabdd4ed5baa55d8496cc675
OfficePC-WSL <jackie@dast.tw>
2024-04-18 12:03:51 +0800

    修正: 在檔案間套用一致的 Python 主機程式設定
    - 移除 `init.lua` 中的設定 `vim.g.python3_host_prog`
    - 在 `vim-options.lua` 中新增設定 `vim.g.python3_host_prog`

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.g.python3_host_prog = "/usr/bin/python3"
 
 vim.g.background = "light"
 
