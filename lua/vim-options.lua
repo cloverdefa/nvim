98c9f12d45ba7ed3f590274595fc4ba5caf47dd6
OfficePC-WSL <jackie@dast.tw>
2024-04-19 12:26:08 +0800

    重構：改進檔案類型識別支援
    - 改變 zsh 和 sh 檔案的檔案類型識別方式

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -20,22 +20,13 @@ vim.wo.number = true
 
 -- make zsh files recognized as sh for bash-ls & treesitter
 
-vim.filetype.add {
-
-	extension = {
-
-		zsh = "sh",
-
-		sh = "sh", -- force sh-files with zsh-shebang to still get sh as filetype
-
-	},
-
-	filename = {
-
-		[".zshrc"] = "sh",
-
-		[".zshenv"] = "sh",
-
-	},
-
-}
+vim.filetype.add({
+  extension = {
+    zsh = "sh",
+    sh = "sh", -- force sh-files with zsh-shebang to still get sh as filetype
+  },
+  filename = {
+    [".zshrc"] = "sh",
+    [".zshenv"] = "sh",
+  },
+})
