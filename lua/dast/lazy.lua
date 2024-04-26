69713555e5deb341ed150011f955793fb9d38b55
HomePC-WSL <jackie@dast.tw>
2024-04-26 19:04:52 +0800

    功能: 合併插件配置更新
    - 更新 lazy.lua 以使用 josean.plugins 初始化
    - 添加一個新文件 bufferline.lua，包含 bufferline 插件的配置
    
    Signed-off-by: HomePC-WSL <jackie@dast.tw>

--- lua/dast/lazy.lua
+++ lua/dast/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("dast.plugins")
+require("lazy").setup("josean.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
