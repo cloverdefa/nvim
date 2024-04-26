5ced3082b8ba91218be4ab3519ad473c346b256f
HomePC-WSL <jackie@dast.tw>
2024-04-26 20:30:07 +0800

    特性: 配置 `mason` 插件並設置 `lazy` 依賴項
    - 更新 `lazy.lua` 以設置 `lazy` 並加入額外的插件導入
    - 在 `dast/plugins/lsp/` 目錄中新增 `mason.lua` 並配置 `mason` 和 `mason-lspconfig`
    
    Signed-off-by: HomePC-WSL <jackie@dast.tw>

--- lua/dast/lazy.lua
+++ lua/dast/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("dast.plugins", {
+require("lazy").setup({ { import = "dast.plugins" }, { import = "dast.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
