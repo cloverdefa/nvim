7912018a494151c844a62afac16da2b02bda15aa
HomePC-WSL <jackie@dast.tw>
2024-04-28 18:41:34 +0800

    翻譯完成:
    工作：在 `keymaps.lua` 中更新按鍵綁定及翻譯
    
    - 為分割視窗更新 `keymaps.lua` 中的按鍵綁定
    - 將 `keymaps.lua` 中的按鍵綁定描述翻譯成中文
    
    Signed-off-by: HomePC-WSL <jackie@dast.tw>

--- lua/dast/core/keymaps.lua
+++ lua/dast/core/keymaps.lua
@@ -20,13 +20,13 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incremen
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- 右方開啟分割視窗
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- 下方開啟分割視窗
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- 還原分割視窗布局回到初始尺寸
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- 關閉分割是裝
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- 開啟新分頁
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- 關閉目前分頁
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  移動到下一個分頁
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  移動到前一個分頁
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  移動目前緩衝區至新分頁
