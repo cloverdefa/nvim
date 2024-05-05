0f5d19e6a6f044e3436f91811fb3dedf3b1de14c
HomePC-WSL <jackie@dast.tw>
2024-05-05 19:24:00 +0800

    修改timeout

--- lua/dast/plugins/formatting.lua
+++ lua/dast/plugins/formatting.lua
@@ -33,7 +33,7 @@ return {
       conform.format({
         lsp_fallback = true,
         async = false,
-        timeout_ms = 2000,
+        timeout_ms = 1000,
       })
     end, { desc = "Format file or range (in visual mode)" })
   end,
