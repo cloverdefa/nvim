5c302b1cbdcb951184792a9ae863e2c8708f3427
OfficePC-WSL <jackie@dast.tw>
2024-05-10 07:56:25 +0800

    修正：更新程式碼檢查工具和語言伺服器插件的設定
    - 修正 zsh 插件名稱中的拼字錯誤，從 &#34;bueatysh&#34; 修改為 &#34;beautysh&#34;
    - 將 &#34;beautysh&#34; 添加為 zsh 的程式碼檢查工具
    - 修改 lspconfig 插件中針對 PowerShell 的檔案類型分配
    
    Signed-off-by: OfficePC-WSL <jackie@dast.tw>

--- lua/dast/plugins/formatting.lua
+++ lua/dast/plugins/formatting.lua
@@ -13,7 +13,7 @@ return {
         yaml = { "prettier" },
         lua = { "stylua" },
         python = { "isort", "black" },
-        zsh = { "bueatysh" },
+        zsh = { "beautysh" },
       },
       format_on_save = {
         lsp_fallback = true,
