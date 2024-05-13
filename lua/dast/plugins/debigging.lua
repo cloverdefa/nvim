cf3827ab312c9c2b99f77e2bbc4e3389e80bc61e
OfficePC-WSL <jackie@dast.tw>
2024-05-13 10:45:20 +0800

    Add dap-python

---
+++ lua/dast/plugins/debigging.lua
@@ -0,0 +1,54 @@
+return {
+  "mfussenegger/nvim-dap",
+  event = "VeryLazy",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    dap.adapters.python = function(cb, config)
+      if config.request == "attach" then
+        ---@diagnostic disable-next-line: undefined-field
+        local port = (config.connect or config).port
+        ---@diagnostic disable-next-line: undefined-field
+        local host = (config.connect or config).host or "127.0.0.1"
+        cb({
+          type = "server",
+          port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+          host = host,
+          options = {
+            source_filetype = "python",
+          },
+        })
+      else
+        cb({
+          type = "executable",
+          command = "path/to/virtualenvs/debugpy/bin/python",
+          args = { "-m", "debugpy.adapter" },
+          options = {
+            source_filetype = "python",
+          },
+        })
+      end
+    end
+
+    vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+  end,
+}
