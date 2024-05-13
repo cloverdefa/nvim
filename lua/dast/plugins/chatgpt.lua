f914e237ffd8a54f0e1b29a3230ef3bf9a47c882
HomePC-WSL <jackie@dast.tw>
2024-05-13 16:14:23 +0800

    重構: 重新編寫程式碼以使用一致的本地別名
    - 將 keymap 變數更新為使用本地別名，以提高可讀性
    - 重構程式碼以在整個檔案中一致地使用 `keymap` 別名
    
    Signed-off-by: HomePC-WSL <jackie@dast.tw>

--- lua/dast/plugins/chatgpt.lua
+++ lua/dast/plugins/chatgpt.lua
@@ -9,11 +9,13 @@ return {
   },
   config = function()
     local home = vim.fn.expand("$HOME")
+    local keymap = vim.keymap
+
     require("chatgpt").setup({
       api_key_cmd = "gpg --decrypt " .. home .. "/openai.gpg",
     })
-    vim.keymap.set("n", "<Leader>cC", "<cmd>CHatGPTCompleteCode<CR>", { desc = "ChatGPT AutoComplete Code" })
-    vim.keymap.set("n", "<Leader>ct", "<cmd>ChatGPTRun add_tests<CR>", { desc = "ChatGPT Add Test Code" })
+    keymap.set("n", "<Leader>cC", "<cmd>CHatGPTCompleteCode<CR>", { desc = "ChatGPT AutoComplete Code" })
+    keymap.set("n", "<Leader>ct", "<cmd>ChatGPTRun add_tests<CR>", { desc = "ChatGPT Add Test Code" })
   end,
   key = {
     { "<Leader>cc", "<cmd>ChatGPT<CR>", { desc = "Open ChatGPT" } },
