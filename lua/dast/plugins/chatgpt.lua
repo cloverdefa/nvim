f40df1523aba591c2dd91fcb5f621d30937233cc
OfficePC-WSL <jackie@dast.tw>
2024-05-16 11:50:47 +0800

    GIT COMMIT MESSAGE:
    重構: 重構設定和 API 金鑰配置
    
    - 從一個表格中刪除 `chatgpt` 設置的初始化，現在直接初始化
    - 添加配置變量以更清晰地設置 API 金鑰命令
    
    Signed-off-by: OfficePC-WSL <jackie@dast.tw>

--- lua/dast/plugins/chatgpt.lua
+++ lua/dast/plugins/chatgpt.lua
@@ -1,22 +1,24 @@
 return {
   "jackMort/ChatGPT.nvim",
   -- enabled = false,
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "folke/trouble.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
   config = function()
     local home = vim.fn.expand("$HOME")
 
-    require("chatgpt").setup({
+    local config = {
       api_key_cmd = "gpg --decrypt " .. home .. "/openai.gpg",
-    })
+    }
+
+    require("chatgpt").setup(config)
   end,
   keys = {
     { "<Leader>cc", "<cmd>ChatGPT<CR>", desc = "Open ChatGPT" },
     { "<Leader>cm", "<cmd>ChatGPTCompleteCode<CR>", desc = "ChatGPT AutoComplete Code" },
     { "<Leader>ct", "<cmd>ChatGPTRun add_tests<CR>", desc = "ChatGPT Add Test Code" },
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim",
   },
 }
